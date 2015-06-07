1489706a0c5892ba6568c0e063b0f1ad00917d23
Junior Monta <monta.junior@gmail.com>
2015-06-07 00:46:32 -0700

    Adjusting equality comparision

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ Object.defineProperty(module.exports, 'engine', {
       dust.onLoad = function (name, callback) {
         var content;
 
-        if(typeof options.settings.views == 'string') {
+        if(typeof options.settings.views === 'string') {
           content = fs.readFileSync(path.resolve(options.settings.views, name) + ext).toString();
         }
         else {
